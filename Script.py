e2c46e601f2a993ed48a79169cb9a8817ae741a1
MR-BOTZt <120255966+MR-BOTZt@users.noreply.github.com>
2023-02-11 15:21:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,7 +2,7 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}\n𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
 
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href=https://t.me/UnKn0wN_DeViL>ㅤ𝘌𝘷𝘪𝘭 𝘔𝘰𝘯𝘬</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href=https://t.me/BlessonXd>「 Bʅҽʂʂoɳ 」»「🇮🇳」</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : 𝙼𝙾𝙽𝙶𝙾-𝙳𝙱
@@ -13,7 +13,7 @@ class script(object):
 - 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁 👉 :<a href=https://t.me/TonyStark_tm_Bot>ᴛᴏɴy ꜱᴛᴀʀᴋ</a>
 
 <b>DEVS:</b>
-- 𝙳𝚎𝚟 1<a href=https://t.me/UnKn0wN_DeViL>ㅤ𝘌𝘷𝘪𝘭 𝘔𝘰𝘯𝘬</a>
+- 𝙳𝚎𝚟 1<a href=https://t.me/BlessonXd>「 Bʅҽʂʂoɳ 」»「🇮🇳」</a>
 - 𝙳𝚎𝚟 2<a href=https://t.me/TonyStark_Updates>𝙏𝙊𝙉𝙔 𝙎𝙏𝘼𝙍𝙆 ᵇᵒᵗ</a>"""
 
     FILE_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝐅𝐢𝐥𝐞 𝐒𝐭𝐨𝐫𝐞 𝐌𝐨𝐝𝐮𝐥𝐞../
